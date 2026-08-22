a67f0b8d1d84a46c16954a4a8f4fa4157327921c
Nien098 <rreilers@gmail.com>
2026-08-22 15:41:47 -0700

    Deploy: fix Overpass failure wrongly cached as confirmed-empty; add retry backoff

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"83675ed27633283e7fc296c8bca22e841224c0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3085445861" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1761616553" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
